4e9d331b3c6758fc780e8b2f94e2eef4d5aa000f
Nicholas <s5124083@bournemouth.ac.uk>
2020-05-04 20:51:23 +0100

    Finishing touches.

--- app/src/main/java/uk/ac/bournemouth/ap/dotsandboxes/GameSettingsActivity.kt
+++ app/src/main/java/uk/ac/bournemouth/ap/dotsandboxes/GameSettingsActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.SeekBar
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_game_settings.*
 import org.example.student.dotsboxgame.StudentDotsBoxGame
 
@@ -14,6 +15,9 @@ class GameSettingsActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListene
     lateinit var btn_start_game: Button
     lateinit var bar_width: SeekBar
     lateinit var bar_height: SeekBar
+    lateinit var widthText: TextView
+    lateinit var heightText: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +29,15 @@ class GameSettingsActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListene
 
         bar_width = findViewById(R.id.seekBarWidth)
         bar_height = findViewById(R.id.seekBarHeight)
+        widthText = findViewById(R.id.text_width_label)
+        heightText = findViewById(R.id.text_height_label)
         btn_start_game = findViewById(R.id.btn_start_game)
 
         btn_start_game.setOnClickListener()
         {
             val intent = Intent(this, DotsAndBoxesGameView::class.java)
 
-            myGameView.myGameInstance = StudentDotsBoxGame(bar_width.progress, bar_height.progress,
+            myGameView.myGameInstance = StudentDotsBoxGame(bar_width.progress + 2, bar_height.progress + 2,
                                                            listOf(StudentDotsBoxGame.NamedHumanPlayer("Player 1"), StudentDotsBoxGame.EasyAI("Computer 1")))
 
             startActivity(intent)
@@ -40,11 +46,23 @@ class GameSettingsActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListene
     }
 
     override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-        if(seekBar.id == R.id.seekBarWidth)
+        //Need to fix this so that the second text view gets updated
+        when(seekBar.id)
         {
-            textView.setText("gameSize: ${seekBar.progress + 3}")
+            R.id.seekBarWidth -> widthText.text = "Grid width: ${bar_width.progress + 2}"
+            R.id.seekBarHeight -> heightText.text = "Grid height ${bar_height.progress + 2}"
         }
 
+/*
+        if(seekBar.id == R.id.seekBarWidth)
+        {
+            widthText.text = "Grid width: ${bar_width.progress + 2}"
+        }
+        else if(seekBar.id == R.id.seekBarHeight)
+        {
+            heightText.text = "Grid height ${bar_height.progress + 2}"
+        }
+        */
     }
 
     override fun onStartTrackingTouch(seekBar: SeekBar?) {
